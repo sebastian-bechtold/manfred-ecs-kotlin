f276d12f43f944519bc62e5262032c7e0ccc74fe
sbecht <sbecht>
2018-03-18 09:07:49 +0100

    - Fixed: Caching error. - Fixed: Some warnings. - Changed '.get(index)' calls to [] operator.

--- src/ManfredEcs.kt
+++ src/ManfredEcs.kt
@@ -1,4 +1,4 @@
-// Last change: 2018-02-18
+// Last change: 2018-03-18
 
 package com.sebastianbechtold.vectro
 
@@ -6,47 +6,48 @@ var em = ManfredEcs()
 
 open class ManfredComponent {
 
-    var id : Long = 0
+    var id: Long = 0
 }
 
 
 class ManfredEcs {
 
-    private var _deleteList = ArrayList<Long>()
+    // NOTE: entities must be public since 'getComponent()' must be 'inline fun <reified T>'.
+    var entities = HashMap<Long, HashMap<Any, ManfredComponent>>()
 
-    private var _entities = HashMap<Long, HashMap<Any, ManfredComponent>>()
+    private var _deleteList = HashSet<Long>()
 
-    private var nextId : Long = 0
 
+
+    private var _nextId: Long = 0
     private var _queryCache = HashMap<String, ArrayList<Long>>()
+    private var _useCache = false
 
-    private var _useCache = true
 
     fun getUnusedId(): Long {
-        return nextId++
+        return _nextId++
     }
 
 
-    fun <T> getComponent(id: Long, compClass: Class<T>): T? {
+    inline fun <reified T> getComponent(id: Long, compClass: Class<T>): T? {
 
-        var entity : HashMap<Any, ManfredComponent>? = _entities.get(id)
+        var entity: HashMap<Any, ManfredComponent>? = entities[id]
 
         if (entity == null) {
             return null
         }
 
-        return entity.get(compClass) as T?
+        return entity[compClass] as T?
     }
 
 
     fun finish() {
-        for(id in _deleteList) {
-            _entities.remove(id)
+
+        for (id in _deleteList) {
+            entities.remove(id)
         }
 
         _deleteList.clear()
-
-    //    println(_entities.size)
     }
 
 
@@ -64,7 +65,7 @@ class ManfredEcs {
                 cacheKey += compClass.name + ";"
             }
 
-            var cachedResult = _queryCache.get(cacheKey)
+            val cachedResult = _queryCache[cacheKey]
 
             if (cachedResult != null) {
                 return cachedResult
@@ -72,10 +73,9 @@ class ManfredEcs {
         }
         //############## END Cache lookup #############
 
-
-        for (entry in _entities) {
+        //############# BEGIN Find all entities that have the specified components ###########
+        for (entry in entities) {
             var allIn = true
-
 
             for (compClass in compClasses) {
                 if (!entry.value.containsKey(compClass)) {
@@ -88,6 +88,7 @@ class ManfredEcs {
                 result.add(entry.key)
             }
         }
+        //############# END Find all entities that have the specified components ###########
 
         // Write query result to cache:
         if (_useCache) {
@@ -99,6 +100,7 @@ class ManfredEcs {
 
 
     private fun clearCache(type: Class<*>) {
+
         val dirty = ArrayList<String>()
 
         for (key in _queryCache.keys) {
@@ -113,49 +115,60 @@ class ManfredEcs {
         }
     }
 
-    fun hasEntity(id: Long): Boolean {
-        return _entities.containsKey(id)
-    }
 
     fun <T> removeComponent(id: Long, compClass: Class<T>) {
-        var entity = _entities.get(id)
 
-        if (entity == null) {
-            return
-        }
+        var entity = entities[id]
+        if (entity == null) return
 
-        entity.remove(compClass)
+
+        if (entity.remove(compClass) == null) return
+
 
         clearCache(compClass);
-        
+
         if (entity.isEmpty()) {
-            removeEntity(id)
+            _deleteList.add(id)
         }
     }
 
-    fun removeEntity(id: Long) {
-        _deleteList.add(id)
 
+    fun removeEntity(id: Long) {
+
+        var entity = entities.get(id)
+
+        if (entity == null) return
+
+
+        var componentsToRemove = ArrayList<ManfredComponent>()
+
+        componentsToRemove.addAll(entity.values)
+
+        for (comp in componentsToRemove) {
+            removeComponent(id, comp.javaClass)
+        }
+
+        _deleteList.add(id)
     }
+
 
     fun setComponent(id: Long, comp: ManfredComponent) {
 
-        var entity = _entities.get(id);
+        var entity = entities[id];
 
-        var prevComp : ManfredComponent? = null
+        var prevComp: ManfredComponent? = null
 
         if (entity == null) {
-            entity = HashMap<Any, ManfredComponent>()
-            _entities.set(id, entity)
-        }
-        else {
+            entity = HashMap()
+            entities.set(id, entity)
+        } else {
             prevComp = entity.get(comp::class.java)
         }
 
         comp.id = id
         entity.set(comp::class.java, comp)
 
-        if (prevComp == null) {
+        if (_useCache && prevComp == null) {
             clearCache(comp.javaClass)
         }
     }
