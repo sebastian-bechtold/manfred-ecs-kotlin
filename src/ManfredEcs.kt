3913886fafc962ca447af93575d32c91393d7c89
sbecht <sbecht>
2018-04-24 23:26:47 +0200

    Changes

--- src/ManfredEcs.kt
+++ src/ManfredEcs.kt
@@ -1,4 +1,4 @@
-// Last change: 2018-03-18
+// Last change: 2018-04-23
 
 package com.sebastianbechtold.vectro
 
@@ -10,21 +10,25 @@ abstract class ManfredComponent {
 
 class ManfredEcs {
 
-    // NOTE: entities must be public since 'getComponent()' must be 'inline fun <reified T>'.
-    var entities = HashMap<Long, HashMap<Any, ManfredComponent>>()
-
+    private var _entities = HashMap<Long, HashMap<Any, ManfredComponent>>()
     private var _deleteList = HashSet<Long>()
-
     private var _nextId: Long = 0
+
 
     fun getUnusedId(): Long {
         return _nextId++
     }
 
+    fun entityExists(id: Long): Boolean {
+        return _entities.containsKey(id)// && !_deleteList.contains(id)
+    }
 
-    inline fun <reified T> getComponent(id: Long, compClass: Class<T>): T? {
 
-        var entity: HashMap<Any, ManfredComponent>? = entities[id]
+    fun <T> getComponent(id: Long, compClass: Class<T>): T? {
+
+       // if (_deleteList.contains(id)) return null
+
+        var entity: HashMap<Any, ManfredComponent>? = _entities[id]
 
         if (entity == null) {
             return null
@@ -37,19 +41,44 @@ class ManfredEcs {
     fun finish() {
 
         for (id in _deleteList) {
-            entities.remove(id)
+            _entities.remove(id)
         }
 
         _deleteList.clear()
     }
 
 
-    fun getEntitiesWith(vararg compClasses: Class<*>): HashSet<Long> {
+    fun getEntitiesWithCompInstance(comp: ManfredComponent): HashSet<Long> {
+        var result = HashSet<Long>()
+
+        //############# BEGIN Find all _entities that have the specified components ###########
+        for (entry in _entities) {
+
+            if (_deleteList.contains(entry.key)) {
+          //      continue
+            }
+
+            if (entry.value.containsValue(comp)) {
+                result.add(entry.key)
+
+            }
+        }
+        //############# END Find all _entities that have the specified components ###########
+
+        return result
+    }
+
+
+    fun getEntitiesWithCompClasses(vararg compClasses: Class<*>): HashSet<Long> {
 
         var result = HashSet<Long>()
 
-        //############# BEGIN Find all entities that have the specified components ###########
-        for (entry in entities) {
+        //############# BEGIN Find all _entities that have the specified components ###########
+        for (entry in _entities) {
+
+            if (_deleteList.contains(entry.key)) {
+         //       continue
+            }
 
             var allIn = true
 
@@ -65,7 +94,7 @@ class ManfredEcs {
                 result.add(entry.key)
             }
         }
-        //############# END Find all entities that have the specified components ###########
+        //############# END Find all _entities that have the specified components ###########
 
         return result
     }
@@ -73,11 +102,11 @@ class ManfredEcs {
 
     fun <T> removeComponent(id: Long, compClass: Class<T>) {
 
-        var entity = entities[id]
+        var entity = _entities[id]
         if (entity == null) return
 
         // TODO: 2 Maybe not remove components immediately.
-        // Instead, create a delete list and do the actual removal in "finish()", just like with entities.
+        // Instead, create a delete list and do the actual removal in "finish()", just like with _entities.
         // This needs further testing. sbecht 2018-03-18
 
         entity.remove(compClass)
@@ -90,14 +119,14 @@ class ManfredEcs {
 
     fun removeEntity(id: Long) {
 
-        var entity = entities.get(id)
+        var entity = _entities.get(id)
 
         if (entity == null) return
 
         // ATTENTION:
-        // entity.clear() must NOT BE CALLED here! Reason: "getEntitiesWith()" returns a list of entity IDs, and if
-        // removeEntity() is called within a loop over such a list (i.e. typical "system" behaviour),
-        // the list becomes invalid. sbecht 2018-03-18
+        // entity.clear() must NOT BE CALLED here! Reason: "getEntitiesWithCompClasses()"
+        // returns a list of entity IDs, and if removeEntity() is called within a loop over
+        // such a list (i.e. typical "system" behaviour), the list becomes invalid. sbecht 2018-03-18
 
         _deleteList.add(id)
     }
@@ -105,15 +134,19 @@ class ManfredEcs {
 
     fun setComponent(id: Long, comp: ManfredComponent) {
 
-        var entity = entities[id];
+        var entity = _entities[id];
 
         if (entity == null) {
             entity = HashMap()
-            entities.set(id, entity)
+            _entities.set(id, entity)
         }
 
         comp.id = id
 
         entity.set(comp::class.java, comp)
+    }
+
+    fun undoDelete(id : Long) {
+        _deleteList.remove(id)
     }
 }
