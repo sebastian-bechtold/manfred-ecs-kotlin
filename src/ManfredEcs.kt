7746147d46df4695828d0f13770cd7c9aeec4226
sbecht <sbecht>
2018-05-11 23:13:44 +0200

    Complete rewrite

--- src/ManfredEcs.kt
+++ src/ManfredEcs.kt
@@ -1,97 +1,82 @@
-// Last change: 2018-04-23
+// Last change: 2018-05-11
 
 package com.sebastianbechtold.vectro
 
-var em = ManfredEcs()
+var em = ManfredEntityList()
 
 abstract class ManfredComponent {
-    var id: Long = 0
+
+    open fun onRemove() {
+
+    }
 }
 
-class ManfredEcs {
+class ManfredEntity {
 
-    private var _entities = HashMap<Long, HashMap<Any, ManfredComponent>>()
-    private var _deleteList = HashSet<Long>()
-    private var _nextId: Long = 0
+    internal var components = HashMap<Any, ManfredComponent>()
 
-
-    fun getUnusedId(): Long {
-        return _nextId++
-    }
-
-    fun entityExists(id: Long): Boolean {
-        return _entities.containsKey(id)// && !_deleteList.contains(id)
+    fun <T> getComponent(compClass: Class<T>): T? {
+        return components[compClass] as T?
     }
 
 
-    fun <T> getComponent(id: Long, compClass: Class<T>): T? {
+    fun <T> removeComponent(compClass: Class<T>) {
 
-       // if (_deleteList.contains(id)) return null
+        var comp  = components.get(compClass)
 
-        var entity: HashMap<Any, ManfredComponent>? = _entities[id]
+        if (comp == null) return
 
-        if (entity == null) {
-            return null
+        comp.onRemove()
+
+        components.remove(compClass)
+    }
+
+
+    fun setComponent(comp: ManfredComponent) {
+        components.set(comp::class.java, comp)
+    }
+}
+
+
+class ManfredEntityList : Iterable<ManfredEntity> {
+
+    private var _entities = HashSet<ManfredEntity>()
+
+
+    fun addEntity(entity : ManfredEntity) {
+        _entities.add(entity)
+    }
+
+
+    fun deleteEntity(entity: ManfredEntity) {
+
+        for(comp in entity.components.values) {
+            comp.onRemove()
         }
 
-        return entity[compClass] as T?
+        _entities.remove(entity)
     }
 
 
-    fun finish() {
+    fun getEntitiesWith(vararg compClasses: Class<*>): ManfredEntityList {
 
-        for (id in _deleteList) {
-            _entities.remove(id)
-        }
-
-        _deleteList.clear()
-    }
-
-
-    fun getEntitiesWithCompInstance(comp: ManfredComponent): HashSet<Long> {
-        var result = HashSet<Long>()
+        var result = ManfredEntityList()
 
         //############# BEGIN Find all _entities that have the specified components ###########
-        for (entry in _entities) {
-
-            if (_deleteList.contains(entry.key)) {
-          //      continue
-            }
-
-            if (entry.value.containsValue(comp)) {
-                result.add(entry.key)
-
-            }
-        }
-        //############# END Find all _entities that have the specified components ###########
-
-        return result
-    }
-
-
-    fun getEntitiesWithCompClasses(vararg compClasses: Class<*>): HashSet<Long> {
-
-        var result = HashSet<Long>()
-
-        //############# BEGIN Find all _entities that have the specified components ###########
-        for (entry in _entities) {
-
-            if (_deleteList.contains(entry.key)) {
-         //       continue
-            }
+        for (entity in _entities) {
 
             var allIn = true
 
             for (compClass in compClasses) {
 
-                if (!entry.value.containsKey(compClass)) {
+                if (!entity.components.containsKey(compClass)) {
                     allIn = false;
                     break;
                 }
             }
 
             if (allIn) {
-                result.add(entry.key)
+                result._entities.add(entity)
             }
         }
         //############# END Find all _entities that have the specified components ###########
@@ -100,53 +85,18 @@ class ManfredEcs {
     }
 
 
-    fun <T> removeComponent(id: Long, compClass: Class<T>) {
-
-        var entity = _entities[id]
-        if (entity == null) return
-
-        // TODO: 2 Maybe not remove components immediately.
-        // Instead, create a delete list and do the actual removal in "finish()", just like with _entities.
-        // This needs further testing. sbecht 2018-03-18
-
-        entity.remove(compClass)
-
-        if (entity.isEmpty()) {
-            _deleteList.add(id)
-        }
+    override fun iterator(): Iterator<ManfredEntity> {
+        return _entities.iterator()
     }
 
 
-    fun removeEntity(id: Long) {
+    fun newEntity() : ManfredEntity {
 
-        var entity = _entities.get(id)
+        var entity = ManfredEntity()
 
-        if (entity == null) return
+        _entities.add(entity)
 
-        // ATTENTION:
-        // entity.clear() must NOT BE CALLED here! Reason: "getEntitiesWithCompClasses()"
-        // returns a list of entity IDs, and if removeEntity() is called within a loop over
-        // such a list (i.e. typical "system" behaviour), the list becomes invalid. sbecht 2018-03-18
-
-        _deleteList.add(id)
-    }
-
-
-    fun setComponent(id: Long, comp: ManfredComponent) {
-
-        var entity = _entities[id];
-
-        if (entity == null) {
-            entity = HashMap()
-            _entities.set(id, entity)
-        }
-
-        comp.id = id
-
-        entity.set(comp::class.java, comp)
-    }
-
-    fun undoDelete(id : Long) {
-        _deleteList.remove(id)
+        return entity
     }
 }
+
